058f4d75927399fe1d4613f3d896bd86359ab26e
Jeff Nelson <jpnelson85@gmail.com>
2023-10-31 15:48:00 -0500

    add subscription serializer

---
+++ app/serializers/subscription_serializer.rb
@@ -0,0 +1,12 @@
+class SubscriptionSerializer
+  include JSONAPI::Serializer
+  attributes :title, :price_dollars, :frequency_by_months, :role
+
+  attribute :tea do |object|
+    object.tea.title
+  end
+
+  attribute :customer do |object|
+    object.customer.email
+  end
+end
