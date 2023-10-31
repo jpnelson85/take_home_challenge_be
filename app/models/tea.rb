b3ce7ebe8b68ad89d2ca3828e34add50f7ae7149
Jeff Nelson <jpnelson85@gmail.com>
2023-10-30 20:33:39 -0500

    add model tests

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,2 +1,8 @@
 class Tea < ApplicationRecord
+  has_many :subscriptions
+  has_many :customers, through: :subscriptions
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :temperature_f, presence: true
+  validates :brew_time_min, presence: true
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @customer = Customer.create!(first_name: "John", last_name: "Doe", email: "j@gmail.com", address: "123 Main St")
+    @tea = Tea.create!(title: "Green Tea", description: "Green", temperature_f: 200, brew_time_min: 5)
+    @subscription = Subscription.create!(title: "Green Tea", price_dollars: 10, frequency_by_months: 1, customer_id: @customer.id, tea_id: @tea.id)
+  end
+  it { should belong_to(:customer) }
+  it { should belong_to(:tea) }
+  it { should validate_presence_of(:title) }
+  it { should validate_uniqueness_of(:title)}
+  it { should validate_presence_of(:price_dollars)}
+  it { should validate_presence_of(:frequency_by_months)}
+  it { should validate_numericality_of(:frequency_by_months).is_greater_than(0)}
 end
