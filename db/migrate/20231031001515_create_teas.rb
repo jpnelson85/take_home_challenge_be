e6d3d061cd9a6169180755775c68e853c9b829c5
Jeff Nelson <jpnelson85@gmail.com>
2023-10-30 19:49:11 -0500

    create customer subscription and tea databases

---
+++ db/migrate/20231031001515_create_teas.rb
@@ -0,0 +1,12 @@
+class CreateTeas < ActiveRecord::Migration[7.0]
+  def change
+    create_table :teas do |t|
+      t.string :title
+      t.string :description
+      t.integer :temperature_f
+      t.integer :brew_time_min
+
+      t.timestamps
+    end
+  end
+end
