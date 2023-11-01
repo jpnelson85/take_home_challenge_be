2ca26b2a2820fdd862f2cc5949da0f651dd2cbf3
Jeff Nelson <jpnelson85@gmail.com>
2023-11-01 18:10:38 -0500

    update tests

--- spec/requests/subscriptions/cancel_spec.rb
+++ spec/requests/subscriptions/cancel_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 RSpec.describe "Subscription Cancel" do
   before(:each) do
-    @customer = Customer.create!(first_name: "John", last_name: "Doe", email: "jdoe@gmail.com", address: "123 Main St")
-    @tea = Tea.create!(title: "Green Tea", description: "Green", temperature_f: 200, brew_time_min: 5)
+    @customer = Customer.create!(first_name: "John", last_name: "Doe", email: "jandoe@gmail.com", address: "123 Main St")
+    @tea = Tea.create!(title: "Purple Tea", description: "Green", temperature_f: 200, brew_time_min: 5)
     @subscription = Subscription.create!(title: "Green Tea", price_dollars: 10, frequency_by_months: 1, customer_id: @customer.id, tea_id: @tea.id, status: "active")
   end
 
@@ -20,7 +20,7 @@ RSpec.describe "Subscription Cancel" do
   end
 
   it "errors if subscription is not found" do
-    @update_params = { email: "test email", title: "Black Tea", status: "cancelled" }
+    @update_params = { email: "test email", title: @tea.title, status: "active" }
     
     headers = { "CONTENT_TYPE" => "application/json" }
     patch '/api/v1/subscribe', headers: headers, params: @update_params.to_json
--- spec/requests/subscriptions/index_spec.rb
+++ spec/requests/subscriptions/index_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 RSpec.describe "Subscription index Page" do
   before(:each) do
-    @customer = Customer.create!(first_name: "John", last_name: "Doe", email: "jdoe@gmail.com", address: "123 Main St")
-    @tea1 = Tea.create!(title: "Green Tea", description: "Green", temperature_f: 200, brew_time_min: 5)
-    @tea2 = Tea.create!(title: "Red Tea", description: "Red", temperature_f: 300, brew_time_min: 5)
-    @subscription1 = Subscription.create!(title: "Green Tea_jdoe", price_dollars: 10, frequency_by_months: 1, customer_id: @customer.id, tea_id: @tea1.id, status: "active")
-    @subscription2 = Subscription.create!(title: "Red Tea_jdoe", price_dollars: 20, frequency_by_months: 6, customer_id: @customer.id, tea_id: @tea2.id, status: "active")
+    @customer = Customer.create!(first_name: "John", last_name: "Doe", email: "jetdoe@gmail.com", address: "123 Main St")
+    @tea1 = Tea.create!(title: "Purple Tea", description: "Green", temperature_f: 200, brew_time_min: 5)
+    @tea2 = Tea.create!(title: "Orange Tea", description: "Red", temperature_f: 300, brew_time_min: 5)
+    @subscription1 = Subscription.create!(title: "Purple Tea_jdoe", price_dollars: 10, frequency_by_months: 1, customer_id: @customer.id, tea_id: @tea1.id, status: "active")
+    @subscription2 = Subscription.create!(title: "Orange_jdoe", price_dollars: 20, frequency_by_months: 6, customer_id: @customer.id, tea_id: @tea2.id, status: "active")
   end
 
   it "can show all of customer's subscriptions " do
